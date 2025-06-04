65d3fd82eef2c3022b90c8060f6990452c4103ec
andrewzhaozzzz <zyzgoodluck0821@outlook.com>
2025-06-04 00:25:13 -0400

    Update finetune_model.py

--- code/finetune_model.py
+++ code/finetune_model.py
@@ -19,9 +19,11 @@ import torch.nn as nn
 from datasets import Dataset, DatasetDict
 import tokenize_function
 
-def finetune_model(prepared_dataset, paragraph_col, output_name, prepared_dataset_path = False, messages = True, model = 'sentence-transformers/paraphrase-multilingual-mpnet-base-v2',
-                   tokenizer = 'sentence-transformers/paraphrase-multilingual-mpnet-base-v2', num_labels = 1,
-                   mlm_prob = 0.15):
+def finetune_model(prepared_dataset, paragraph_col, output_name, output_dir, prepared_dataset_path = False, messages = True, 
+                   model = 'sentence-transformers/paraphrase-multilingual-mpnet-base-v2',
+                   tokenizer = 'sentence-transformers/paraphrase-multilingual-mpnet-base-v2', 
+                   num_labels = 1, mlm_prob = 0.15, evaluation_strategy="epoch", learning_rate=1e-5, num_train_epochs=1,
+                   weight_decay=0.01, save_steps=100000, per_device_train_batch_size=16, gradient_accumulation_steps=4):
     # Load the dataset prepared for finetuning
     if prepared_dataset_path == True:
         dataset = pd.read_pickle(prepared_dataset)
@@ -38,16 +40,15 @@ def finetune_model(prepared_dataset, paragraph_col, output_name, prepared_datase
 
     if messages == True:
         print("Model and tokenizer ready.")
-
-    def tokenize_function(examples):
-        return tokenizer(examples["text"], padding=True, truncation=True, return_tensors='pt')
-    tokenized_datasets = dataset.map(tokenize_function.tokenize_function, batched = True)
-    tokenized_datasets = tokenized_datasets.remove_columns(["text"])
+    
+    tokenized_datasets = dataset.map(tokenize_function.tokenize_function, fn_kwargs = {"paragraph_col": paragraph_col}, batched = True)
+    tokenized_datasets = tokenized_datasets.remove_columns([paragraph_col])
     tokenized_datasets.set_format("torch")
 
-    print("Data ready.")
+    if messages == True:
+        print("Data ready.")
 
-    model_dir = os.path.join(model_locat_dir, f'{output_name}_final')
+    model_dir = os.path.join(output_dir, f'{output_name}_final')
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     model.to(device)
 
@@ -55,24 +56,22 @@ def finetune_model(prepared_dataset, paragraph_col, output_name, prepared_datase
         print("Starting training...")
 
     training_args = TrainingArguments(
-        output_dir=model_dir,
-        evaluation_strategy="epoch",
-        learning_rate=1e-5,
-        num_train_epochs=1,
-        weight_decay=0.01,
-        save_steps=100000,
-        per_device_train_batch_size=16,  # smaller batch size
-        gradient_accumulation_steps=4,     # accumulate gradients over multiple steps
-        report_to="wandb",
-        logging_steps=10
+        output_dir = model_dir,
+        evaluation_strategy = evaluation_strategy,
+        learning_rate = learning_rate,
+        num_train_epochs = num_train_epochs,
+        weight_decay = weight_decay,
+        save_steps = save_steps,
+        per_device_train_batch_size = per_device_train_batch_size,  # smaller batch size
+        gradient_accumulation_steps = gradient_accumulation_steps,     # accumulate gradients over multiple steps
     )
 
     trainer = Trainer(
-        model=model,
-        args=training_args,
-        train_dataset=tokenized_datasets["train"],
-        eval_dataset=tokenized_datasets["test"],
-        data_collator=data_collator,
+        model = model,
+        args = training_args,
+        train_dataset = tokenized_datasets["train"],
+        eval_dataset = tokenized_datasets["test"],
+        data_collator = data_collator,
     )
 
     # Check for an existing checkpoint to resume from.
@@ -89,12 +88,14 @@ def finetune_model(prepared_dataset, paragraph_col, output_name, prepared_datase
             print(f"Resuming training from checkpoint: {resume_checkpoint}")
 
     trainer.train(resume_from_checkpoint=resume_checkpoint)
-    print("Training complete.")
+    if messages == True:
+        print("Training complete.")
 
     # Save the model and tokenizer.
     if not os.path.exists(model_dir):
         os.makedirs(model_dir)
     model.save_pretrained(model_dir)
     tokenizer.save_pretrained(model_dir)
-    print("Saved Model.")
+    if messages == True:
+        print("Saved Model.")
 
