d1c814b876cde1a0afab95586db266662d7c1a9a
andrewzhaozzzz <zyzgoodluck0821@outlook.com>
2025-07-29 16:02:55 -0400

    Update finetune_model.py

--- code/finetune_model.py
+++ code/finetune_model.py
@@ -16,16 +16,54 @@ import numpy as np
 import torch
 import torch.nn as nn
 from datasets import Dataset, DatasetDict
-import tokenize_function
+import resonant.prepare_dataset
 
 def finetune_model(prepared_dataset, paragraph_col, output_name, output_dir, prepared_dataset_path = False, messages = True,
                    model = 'sentence-transformers/paraphrase-multilingual-mpnet-base-v2',
                    tokenizer = 'sentence-transformers/paraphrase-multilingual-mpnet-base-v2',
                    num_labels = 1, mlm_prob = 0.15, evaluation_strategy="epoch", learning_rate=1e-5, num_train_epochs=1,
                    weight_decay=0.01, save_steps=100000, per_device_train_batch_size=16, gradient_accumulation_steps=4):
+    """Fine-tuning of a specified LLM that would be used to generate embeddings for text.
+
+    Parameters
+    ----------
+    prepared_dataset : string, DatasetDict
+        If prepared_dataset_path is True, then it should be a string indicating the path of the pickle file containing the original dataset.
+        If prepared_dataset_path is False, then it should be a DatasetDict containing a prepared dataset generated with the function prepare_dataset.
+
+    paragraph_col : string
+        The name of the column corresponding to text or paragraphs we aim to analyze.
+
+    output_name : string
+        A self-selected name for the output model and tokenizer.
+
+    output_dir: string
+        The self-selected path directory for saving the output model and tokenizer.
+        
+    prepared_dataset_path : bool, optional
+        If set to True, then prepared_dataset would read in a pickle file containing the original dataset without preparation.
+    
+    sample_num : int, optional
+        Only needed if spot_check is set to True. The number of samples to perform spot check on.
+
+    random_state : int, optional
+        Only needed if spot_check is set to True. The random state to randomly select samples during spot check.
+
+    num_paras : int, optional
+        If specified, the dataset would be sliced to include only the specified number of rows.
+
+    messages : bool, optional
+        If set to False, then messages that indicate running progress would not be printed out.
+
+    Return
+    -------
+    dataset : DatasetDict
+        A dataset containing training set and test set, based on the original dataset and prepared for LLM fine-tuning.
+    """
     # Load the dataset prepared for finetuning
     if prepared_dataset_path == True:
         dataset = pd.read_pickle(prepared_dataset)
+        dataset = prepare_dataset.prepare_dataset(dataset)
     else:
         dataset = prepared_dataset
 
